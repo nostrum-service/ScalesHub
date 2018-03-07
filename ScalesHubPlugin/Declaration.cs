f4da8c83d42b9ff02e98f601bcdffa7df69927b6
Kirill <cyril.host1010@gmail.com>
2018-03-07 17:22:18 +0300

    Add files via upload

---
+++ ScalesHubPlugin/Declaration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace ScalesHubPlugin
+{
+    /// <summary>
+    /// Для экспорта декодера
+    /// </summary>
+    public interface IDataFrame
+    {
+        bool Decode(byte[] data);
+        NxDeviceState State { get; }
+        double Weight { get; }
+    }
+    /// <summary>
+    /// Метаданные декодера
+    /// </summary>
+    public interface IDataFrameMetadata
+    {
+        string Name { get; }
+        string Description { get; }
+    }    
+}
